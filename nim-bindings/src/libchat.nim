d80f608a6f94af5e066e81d992042e8ba611bc0e
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-01-26 09:58:42 -0800

    remove unneeded code

--- nim-bindings/src/libchat.nim
+++ nim-bindings/src/libchat.nim
@@ -30,67 +30,6 @@ proc destroy*(ctx: var LibChat) =
 proc getBuffer*(ctx: LibChat): seq[byte] =
   newSeq[byte](ctx.buffer_size)
 
-proc sendContent*(ctx: LibChat, conversationId: string, content: openArray[uint8]): seq[PayloadResult] =
-  ## Send content and get back payloads to deliver
-  const
-    maxPayloads = 10
-    payloadMaxLen = 4096
-    addrMaxLen = 256
-
-  # Allocate buffers for payload data
-  var payloadBuffers: array[maxPayloads, array[payloadMaxLen, uint8]]
-  var payloadPtrs: array[maxPayloads, ptr uint8]
-  for i in 0..<maxPayloads:
-    payloadPtrs[i] = addr payloadBuffers[i][0]
-
-  # Allocate buffers for addresses
-  var addrBuffers: array[maxPayloads, array[addrMaxLen, char]]
-  var addrPtrs: array[maxPayloads, ptr cchar]
-  for i in 0..<maxPayloads:
-    addrPtrs[i] = addr addrBuffers[i][0]
-
-  var actualLengths: array[maxPayloads, csize_t]
-  var maxLen: csize_t = payloadMaxLen
-
-  let contentPtr = if content.len > 0: unsafeAddr content[0] else: nil
-
-  let numPayloads = generate_payload(
-    ctx.handle,
-    conversationId.cstring,
-    contentPtr,
-    content.len.csize_t,
-    maxPayloads.csize_t,
-    addr addrPtrs[0],
-    addrMaxLen.csize_t,
-    addr payloadPtrs[0],
-    addr maxLen,
-    addr actualLengths[0]
-  )
-
-  if numPayloads < 0:
-    case numPayloads:
-    of ErrBadPtr:
-      raise newException(ValueError, "Bad pointer passed to generate_payload")
-    of ErrBadConvoId:
-      raise newException(ValueError, "Bad conversation ID: " & conversationId)
-    else:
-      raise newException(IOError, "Unknown error: " & $numPayloads)
-
-  result = @[]
-  for i in 0..<numPayloads:
-    var payload = PayloadResult()
-
-    # Extract address (null-terminated string)
-    payload.address = $cast[cstring](addr addrBuffers[i][0])
-
-    # Extract payload data
-    let dataLen = actualLengths[i].int
-    payload.data = newSeq[uint8](dataLen)
-    if dataLen > 0:
-      copyMem(addr payload.data[0], addr payloadBuffers[i][0], dataLen)
-
-    result.add(payload)
-
 proc setBufferSize*(ctx: var LibChat, buffer_size: uint32) =
   if ctx.handle != nil:
     # Update local value
