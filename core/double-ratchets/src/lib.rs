b6a6a2b76ed3dac84fceff59f5cf7211ede0df36
kaichaosun <kaichaosuna@gmail.com>
2026-04-10 14:30:53 +0800

    chore: remove ffi from double ratchet

--- core/double-ratchets/src/lib.rs
+++ core/double-ratchets/src/lib.rs
@@ -1,6 +1,5 @@
 pub mod aead;
 pub mod errors;
-pub mod ffi;
 pub mod hkdf;
 pub mod keypair;
 pub mod reader;
@@ -11,5 +10,5 @@ pub mod types;
 pub use keypair::InstallationKeyPair;
 pub use state::{Header, RatchetState, SkippedKey};
 pub use storage::{
-    RatchetSession, SessionError, restore_ratchet_state, to_ratchet_record, to_skipped_key_records,
+    restore_ratchet_state, to_ratchet_record, to_skipped_key_records, RatchetSession, SessionError,
 };
