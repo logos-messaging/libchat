cba31be9e0c75f00fdac9b03362c23b085a83212
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-06-23 23:06:48 -0700

    fix imports

--- extensions/components/src/lib.rs
+++ extensions/components/src/lib.rs
@@ -5,8 +5,6 @@ mod wakeup;
 
 pub use contact_registry::ephemeral::EphemeralRegistry;
 pub use contact_registry::http::{HttpRegistry, HttpRegistryError};
+pub use delivery::*;
 pub use storage::*;
 pub use wakeup::*;
-
-#[cfg(logos_delivery)]
-pub use delivery::{EmbeddedP2pDeliveryService, P2pConfig};
