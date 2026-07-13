fdad104129208569542aa7092570fbd14571f5be
Jazz Turner-Baggs <473256+jazzz@users.noreply.github.com>
2026-07-12 17:05:03 -0700

    Update cargo.toml + fixups

--- extensions/logos-delivery-rust/src/lib.rs
+++ extensions/logos-delivery-rust/src/lib.rs
@@ -2,6 +2,4 @@ mod sys;
 mod threaded;
 mod wrapper;
 
-pub use threaded::{
-    DeliveryError, P2pConfig, ReceivedMessage, ThreadedDeliveryWrapper, WakuEvent,
-};
+pub use threaded::{DeliveryError, P2pConfig, ReceivedMessage, ThreadedDeliveryWrapper, WakuEvent};
